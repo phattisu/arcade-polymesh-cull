b7d29141a853471688f987efdc1f07fd04b02cc5
Phattisu <naphatism@hotmail.com>
2025-06-26 16:52:27 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -181,16 +181,16 @@ namespace Polymesh {
             }
         }
         // Draw debug canvas
-        if (debug && debug > 0) {
-            for (const t of tris) {
-                const inds = t.indices;
-                helpers.imageDrawLine(image, rotated[inds[0]].x, rotated[inds[0]].y, rotated[inds[1]].x, rotated[inds[1]].y, debug);
-                helpers.imageDrawLine(image, rotated[inds[0]].x, rotated[inds[0]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
-                if (inds.length > 3) {
-                    helpers.imageDrawLine(image, rotated[inds[3]].x, rotated[inds[3]].y, rotated[inds[1]].x, rotated[inds[1]].y, debug);
-                    helpers.imageDrawLine(image, rotated[inds[3]].x, rotated[inds[3]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
-                } else helpers.imageDrawLine(image, rotated[inds[1]].x, rotated[inds[1]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
-            }
+        if (!debug) return;
+        if (debug <= 0) return;
+        for (const t of tris) {
+            const inds = t.indices;
+            helpers.imageDrawLine(image, rotated[inds[0]].x, rotated[inds[0]].y, rotated[inds[1]].x, rotated[inds[1]].y, debug);
+            helpers.imageDrawLine(image, rotated[inds[0]].x, rotated[inds[0]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
+            if (inds.length > 3) {
+                helpers.imageDrawLine(image, rotated[inds[3]].x, rotated[inds[3]].y, rotated[inds[1]].x, rotated[inds[1]].y, debug);
+                helpers.imageDrawLine(image, rotated[inds[3]].x, rotated[inds[3]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
+            } else helpers.imageDrawLine(image, rotated[inds[1]].x, rotated[inds[1]].y, rotated[inds[2]].x, rotated[inds[2]].y, debug);
         }
     }
 
